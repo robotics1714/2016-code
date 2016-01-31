65adb0bd4d3ff651d0ee7d2f51f97a1c4eb4f413
ColinMercer25 <colinmercer.chen@gmail.com>
2016-02-01 01:51:09 +0900

    commends+3 new method(unfinished)
    added commend for description
    add method to move roller arm to a designated position and tilting
    roller arm up and down for a certain time/distance

--- src/org/usfirst/frc/team1714/robot/RollerClaw.java
+++ src/org/usfirst/frc/team1714/robot/RollerClaw.java
@@ -9,7 +9,11 @@ public class RollerClaw {
 	private DigitalInput backSafetyLS, frontSafetyLS, ballDetectLS;
 	private AnalogPotentiometer rollerPot;
 	private boolean rollerArmUp, rollerArmDown, rollerBarIn, rollerBarOut;
-	private final double armSpeed=0,rollerSpeed=0, rollerPotMax=0, rollerPotMin=0;
+	private final double armSpeed=0,rollerSpeed=0;
+	private final double rollerPotMax=0, rollerPotMin=0;//max and min of potentiometer reading
+	private final double rollerPotPos1=0, rollerPotPos2=0;//range of potentiometer reading for a position
+	
+	
 	
 	RollerClaw(){
 		rollerMotor = new Talon(1);
@@ -20,98 +24,112 @@ public class RollerClaw {
 		rollerPot = new AnalogPotentiometer(1);
 	}
 	
-	private void TiltRollerArmUp(){
-		if(!RollerArmFullUp()){
+	private void tiltRollerArmUp(){//called to perform the action of tilting roller arm up
+		if(!rollerArmFullUp()){
 			armMotor.set(armSpeed);
 		}
 		else{
 			rollerArmUp=false;
 			armMotor.set(0);
-		}
+		}//when it reach the limit it stop
 	}
 	
-	private void TiltRollerArmDown(){
-		if(!RollerArmFullDown()){
+	private void tiltRollerArmDown(){//called to perform the action of tilting roller arm down
+		if(!rollerArmFullDown()){
 			armMotor.set(-armSpeed);
 		}
 		else{
 			armMotor.set(0);
 			rollerArmDown=false;
-		}
+		}//when it reach the limit it stop
 	}
 	
-	private void RollBallIn(){
-		if(!BallAcquired()){
+	private void rollBallIn(){//called to perform the action of rolling the ball in
+		if(!ballAcquired()){
 			rollerMotor.set(rollerSpeed);
 		}
 		else{
 			rollerMotor.set(0);
 			rollerBarIn=false;
-		}
+		}//it stop when the ball reach its safe holding position
 	}
 	
-	private void RollBallOut(){
-		rollerMotor.set(-rollerSpeed);
-		
-	}
+	private void rollBallOut(){//called to perform the action of rolling the ball out
+		rollerMotor.set(-rollerSpeed);	
+	}//this action will not automatically stop till you set inward or stop the roller bar
 	
-	public boolean RollerArmFullUp(){
+	public boolean rollerArmFullUp(){
 		return !backSafetyLS.get();
-	}
+	}//limit switch to prevent roller arm from tilting backward too much
 	
-	public boolean RollerArmFullDown(){
+	public boolean rollerArmFullDown(){
 		return !frontSafetyLS.get();
-	}
+	}//limit switch to prevent roller arm from tilting forward too much
 	
-	public boolean BallAcquired(){
+	public boolean ballAcquired(){
 		return !ballDetectLS.get();
-	}
+	}//limit switch to detect if the ball is hold safely
 	
 	public void Update(){
 		if (rollerArmUp){
-			TiltRollerArmUp();
+			tiltRollerArmUp();
 		}
 		else if(rollerArmDown){
-			TiltRollerArmDown();
+			tiltRollerArmDown();
 		}
 		
 		if(rollerBarIn){
-			RollBallIn();
+			rollBallIn();
 		}
 		else if(rollerBarOut){
-			RollBallOut();
+			rollBallOut();
 		}
+	/*This method is called every time autonomousPeriodic and teleopPeriodic is called
+	 * to check the current status of roller claw
+	 * and send command for actions
+	*/
 	}
 	
-	public void SetRollerArmUp(){
+	public void setRollerArmUp(){
 		rollerArmUp=true;
 		rollerArmDown=false;
-		
-	}
+	}//called to set the roller arm tilt up till it reach the limit
 	
-	public void SetRollerArmDown(){
+	public void setRollerArmDown(){
 		rollerArmDown=true;
 		rollerArmUp=false;
-	}
+	}//called to set the roller arm tilt down till it reach the limit
 	
-	public void SetRollerArmStop(){
+	public void setRollerArmStop(){
 		rollerArmUp=false;
 		rollerArmDown=false;
-	}
+	}//called to stop the roller arm tilting
 	
-	public void SetRollerBarIn(){
+	public void setRollerArmPos(){
+		
+	}//called to set the roller arm to a designated position
+	
+	public void setRollerArmUpward(){
+		
+	}//called to tilt the roller arm up for a certain and designated distance or time
+	
+	public void setRollerArmDownward(){
+		
+	}//called to tilt the roller arm down for a certain and designated distance or time
+	
+	public void setRollerBarIn(){
 		rollerBarIn=true;
 		rollerBarOut=false;
-	}
+	}//called to set the roller bar to roll in the ball
 	
-	public void SetRollerBarOut(){
+	public void setRollerBarOut(){
 		rollerBarOut=true;
 		rollerBarIn=false;
-	}
+	}//called to set the roller bar to roll out the ball
 	
-	public void SetRollerBarStop(){
+	public void setRollerBarStop(){
 		rollerBarIn=false;
 		rollerBarOut=false;
-	}
+	}//called to stop the roller bar rolling
 	
 }
