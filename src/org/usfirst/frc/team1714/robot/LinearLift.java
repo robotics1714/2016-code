6606a6d231058d49c8f943bcf63093d43cf19f63
MORE Robotics Team 1714 <robotics1714@gmail.com>
2016-01-31 11:12:31 -0600

    Implementing Tilting Function
    * Fixed Coding style
    * Added variables to lift control
    * Added false statements to void setTiltLifeUp and setTiltLifeDown
    * Fixed typos for tiltLiftUp and tiltLiftDown
    * Added tiltLiftStop

--- src/org/usfirst/frc/team1714/robot/LinearLift.java
+++ src/org/usfirst/frc/team1714/robot/LinearLift.java
@@ -1,10 +1,10 @@
 package org.usfirst.frc.team1714.robot;
+
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
 
-public class LinearLift
-{
+public class LinearLift {
 	private Talon tiltMotor;
 	private Talon winchMotor;
 	private DigitalInput tiltSafetyLS;
@@ -13,57 +13,55 @@ public class LinearLift
 	private AnalogPotentiometer tiltPot;
 	private AnalogPotentiometer winchPot;
 	final private double tiltSpeed = 0;
+	final private double tiltPotMax = 0;
+	final private double tiltPotMin = 0;
 	final private double winchSpeed = 0;
 	final private double winchPotMax = 0;
 	final private double winchPotMin = 0;
-	private boolean tiltingLiftUp;
-	private boolean tiltingLiftDown;
-	
-	void setTiltLiftUp()
-	{
-		if(!tiltingLiftDown)
-		{
-			tiltingLiftUp = true;
-		}
+	private boolean tiltingLiftUp = false;
+	private boolean tiltingLiftDown = false;
+	private boolean extendLift = false;
+	private boolean retractLift = false;
+
+	void setTiltLiftUp() {
+		tiltingLiftDown = false;
+		tiltingLiftUp = true;
 	}
-	
-	void setTiltLiftDown()
-	{
-		if(!tiltingLiftUp)
-		{
-			tiltingLiftDown = true;
-		}
+
+	void setTiltLiftDown() {
+		tiltingLiftUp = false;
+		tiltingLiftDown = true;
 	}
-	
-	private void tiltLiftUp()
-	{
-		if(tiltSafetyLS.get() || winchPot.get() < winchPotMax )
-		{
-			winchMotor.set(winchSpeed);
-		}
-		else
-		{
-			winchMotor.set(0.0);
+
+	void setTiltLiftStop() {
+		tiltingLiftUp = false;
+		tiltingLiftDown = false;
+		tiltMotor.set(0.0);
+	}
+
+	private void tiltLiftUp() {
+		if (tiltSafetyLS.get() || tiltPot.get() < tiltPotMax) {
+			tiltMotor.set(tiltSpeed);
+		} else {
+			tiltMotor.set(0.0);
 			tiltingLiftUp = false;
 		}
 	}
-	
-	private void tiltLiftDown()
-	{
-		if(winchPot.get() > winchPotMin );
-		{
-			
+
+	private void tiltLiftDown() {
+		if (tiltPot.get() > tiltPotMin) {
+			tiltMotor.set(-tiltSpeed);
+		} else {
+			tiltMotor.set(0.0);
+			tiltingLiftDown = false;
 		}
 	}
-	
-	void update()
-	{
-		if(tiltingLiftUp)
-		{
+
+	void update() {
+		if (tiltingLiftUp) {
 			tiltLiftUp();
 		}
-		if(tiltingLiftDown)
-		{
+		if (tiltingLiftDown) {
 			tiltLiftDown();
 		}
 	}
