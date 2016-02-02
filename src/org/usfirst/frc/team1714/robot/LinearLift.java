21dea906e402440770aebccf520b14be6f35f86f
MORE Robotics Team 1714 <robotics1714@gmail.com>
2016-02-01 19:26:43 -0600

    New servo tilt system
    commented out the entire cim system :^(

--- src/org/usfirst/frc/team1714/robot/LinearLift.java
+++ src/org/usfirst/frc/team1714/robot/LinearLift.java
@@ -3,45 +3,49 @@ package org.usfirst.frc.team1714.robot;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Servo;
 
 // TODO: replace booleans with enum state system
 
 public class LinearLift {
-	private Talon tiltMotor;
+	//CIM TILT SYSTEM// private Talon tiltMotor;
+	private Servo tiltServo;
 	private Talon winchMotor;
 	private DigitalInput tiltLS;
 	private DigitalInput winchLSMax;
 	private DigitalInput winchLSMin;
-	private AnalogPotentiometer tiltPot;
+	//CIM TILT SYSTEM// private AnalogPotentiometer tiltPot;
 	private AnalogPotentiometer winchPot;
 	
-	// THESE ARE PLACEHOLDERS!!! CHANGE THEM!!!
+// THESE ARE PLACEHOLDERS!!! CHANGE THEM!!!
 	
-	final private double tiltSpeed = 0;
-	final private double tiltPotMax = 0;
-	final private double tiltPotMin = 0;
+	//CIM TILT SYSTEM// final private double tiltSpeed = 0;
+	//CIM TILT SYSTEM// final private double tiltPotMax = 0;
+	//CIM TILT SYSTEM// final private double tiltPotMin = 0;
 	final private double winchSpeed = 0;
 	final private double winchPotMax = 0;
 	final private double winchPotMin = 0;
-	final private int tiltPotPin = 0;
-	final private int tiltMotorPin = 0;
+	//CIM TILT SYSTEM// final private int tiltPotPin = 0;
+	final private int tiltServoPin = 0;
 	final private int tiltLSPin = 0;
 	final private int winchPotPin = 0;
 	final private int winchMotorPin = 0;
 	final private int winchLSMaxPin = 0;
 	final private int winchLSMinPin = 0;
+	final private double tiltServoPos = 0;
 	
-	// END OF PLACEHOLDERS!!!
+// END OF PLACEHOLDERS!!!
 	
 	private boolean tiltingLiftUp = false;
-	private boolean tiltingLiftDown = false;
+	//CIM TILT SYSTEM// private boolean tiltingLiftDown = false;
 	private boolean extendingLift = false;
 	private boolean retractingLift = false;
 	
 	LinearLift() {
-		tiltMotor = new Talon(tiltMotorPin);
+		//CIM TILT SYSTEM// tiltMotor = new Talon(tiltMotorPin);
+		tiltServo = new Servo(tiltServoPin);
 		tiltLS = new DigitalInput(tiltLSPin);
-		tiltPot = new AnalogPotentiometer(tiltPotPin);
+		//CIM TILT SYSTEM// tiltPot = new AnalogPotentiometer(tiltPotPin);
 		winchMotor = new Talon(winchMotorPin);
 		winchLSMax = new DigitalInput(winchLSMaxPin);
 		winchLSMin = new DigitalInput(winchLSMinPin);
@@ -49,20 +53,22 @@ public class LinearLift {
 	}
 	
 	void setTiltLiftUp() {
-		tiltingLiftDown = false;
+		//CIM TILT SYSTEM// tiltingLiftDown = false;
 		tiltingLiftUp = true;
 	}
 
+	/*CIM TILT SYSTEM//
 	void setTiltLiftDown() {
 		tiltingLiftUp = false;
 		tiltingLiftDown = true;
 	}
-
+	
 	void setTiltLiftStop() {
 		tiltingLiftUp = false;
 		tiltingLiftDown = false;
 		tiltMotor.set(0.0);
 	}
+	//CIM TILT SYSTEM*/
 	
 	void setExtendLift() {
 		retractingLift = false;
@@ -81,15 +87,17 @@ public class LinearLift {
 	}
 	
 	private void tiltLiftUp() {
-		if (tiltLS.get() && tiltPot.get() < tiltPotMax) {
-			tiltMotor.set(tiltSpeed);
+		if (tiltLS.get() /*CIM TILT SYSTEM// && tiltPot.get() < tiltPotMax */) {
+			//CIM TILT SYSTEM// tiltMotor.set(tiltSpeed);
+			tiltServo.set(tiltServoPos);
 		} 
 		else {
-			tiltMotor.set(0.0);
+			//CIM TILT SYSTEM// tiltMotor.set(0.0);
 			tiltingLiftUp = false;
 		}
 	}
 
+	/*CIM TILT SYSTEM//
 	private void tiltLiftDown() {
 		if (tiltPot.get() > tiltPotMin) {
 			tiltMotor.set(-tiltSpeed);
@@ -99,6 +107,7 @@ public class LinearLift {
 			tiltingLiftDown = false;
 		}
 	}
+	//CIM TILT SYSTEM*/
 	
 	private void extendLift() {
 		if (winchLSMax.get() && winchPot.get() < winchPotMax) {
@@ -125,9 +134,11 @@ public class LinearLift {
 			tiltLiftUp();
 		}
 		
+		/*CIM TILT SYSTEM//
 		if (tiltingLiftDown) {
 			tiltLiftDown();
 		}
+		//CIM TILT SYSTEM*/
 		
 		if (extendingLift) {
 			extendLift();
